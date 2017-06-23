3f763876fad1687200b87c709253899464b156b7
CodHeK <gaganganapathyas@yahoo.in>
2017-06-23 19:43:51 +0530

    yoyo

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 		$("#msg1").val('');
 		var res = val1.match(/up/g);
 		if(res) {
-			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspAhh! Thats not easy to answer... Well, nothings happening in my world, <br>what about you ?</h3></div></li>');	
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspWell, nothings happening in my world, <br>what about you ?</h3></div></li>');	
 		}
 		var res1 =val1.match(/creat/g);
 		var res4 = val1.match(/you/g);
@@ -47,14 +47,33 @@ $(document).ready(function() {
 			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspWell, I was born or created, whatever on 23/5/2017</h3></div></li>');
 		}
 
-		else {
-			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspAhh ! Whut ... seems alien to me, come again please !</h3></div></li>');
-		}
+		
 		var res3a =val1.match(/what/g);
 		var res3b =val1.match(/doing/g);
 		if(res3a && res3b) {
 			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspSeriously?? You ask that to a bot ! :O Kyaa Yaaar! Well, if you really want to know, Im idle ! doing, nothing ! :# Ask me something interesting manh! </h3></div></li>');
 		}
+		var res4a =val1.match(/how/g);
+		var res4b =val1.match(/are/g);
+		var res4c = val1.match(/you/g);
+		if(res4a && res4b && res4c) {
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbsp</h3></div></li>');
+		}
+		var res6 = val1.match(/ridam/g);
+		var no = val1.match(/no/g);
+		var yes = val1.match(/yes/g);
+		if(res6) {
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspYou really wanna talk about her ? eh ?</h3></div></li>');
+		}
+		if(no) {
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspYoure a nice person ! :D </h3></div></li>');
+		}
+		if(yes) {
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbsp *angry face* Its better if we dont ! Get rid of it !</h3></div></li>');
+		}
+		// if(!res1 && !res2 && !res3 && !res4 && !res5 && !res6 && !res2a && !res2b && !res2c && !res2d && !res3a && !res3b && !res4a && !res4b && !res4c && !res && !no && !yes) {
+		// 	$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspAhh ! Whut ... seems alien to me, come again please !</h3></div></li>');
+		// }
 				window.scrollBy(0,2000);
 		event.preventDefault();
 	});
