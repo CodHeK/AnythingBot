e0d2cca85e2be26e1a6c1ad7e14a2dee03172198
CodHeK <gaganganapathyas@yahoo.in>
2017-06-23 16:51:02 +0530

    few more func added

--- bot.js
+++ bot.js
@@ -14,10 +14,18 @@ $(document).ready(function() {
 		$("#form1").show();
 		e.preventDefault();
 	});
-	$("#buttons1").click(function(e) {
+	$("#buttons1").click(function(event) {
 		var val1 = $("#msg1").val();
 		$("#list").append('<li> <div class="jumbotron"><h3><b>'+ name + ':</b>&nbsp' + val1 +'</h3></div></li>');
 		$("#msg1").val('');
-		e.preventDefault();
+		var res = val1.match(/up/g);
+		if(res) {
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspAhh! Thats not easy to answer... Well, nothings happening in my world, what about you ?</h3></div></li>');	
+		}
+		var res1 =val1.match(/creat/g);
+		if(res1) {
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspWell, he himself is called Gags ! :p</h3></div></li>');	
+		}
+		event.preventDefault();
 	});
 });
