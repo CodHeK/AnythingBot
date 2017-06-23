516e01b33af80f7aa543db599cb5051d095dbccc
CodHeK <gaganganapathyas@yahoo.in>
2017-06-23 17:10:12 +0530

    date feat added

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
 		var val = $("#msg").val();
 		$("#list").append('<li> <div class="jumbotron"><h3>' + val +'</h3></div></li>');
 		name = $("li:nth-child(2) .jumbotron h3").html();
-		var introSen = "Feel free too ask me anything, anytime, I'll be always there for you  " + name + "! ;)";
+		var introSen = "" + name + "&nbspfeel free too ask me anything, anytime!";
 		if(name) {
-			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspHey&nbsp' + name +'!</h3></div></li>');	
-			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbsp' + introSen +'!</h3></div></li>');	
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspHey&nbsp' + name +'!</h3></div></li>').fadeIn(2000);
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbsp' + introSen +'!</h3></div></li>').fadeIn(2000);
 		}
 		$("#form").hide();
 		$("#form1").show();
@@ -25,6 +25,11 @@ $(document).ready(function() {
 		var res1 =val1.match(/creat/g);
 		if(res1) {
 			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspWell, he himself is called Gags ! :p</h3></div></li>');	
+		}
+		var res2 =val1.match(/date/g);
+		var date = new Date();
+		if(res2) {
+			$("#list").append('<li> <div class="jumbotron"><h3><b>Gags:</b>&nbspIt is&nbsp' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + ' today, hope youre not missing someones birthday :p !</h3></div></li>');	
 		}
 		event.preventDefault();
 	});
